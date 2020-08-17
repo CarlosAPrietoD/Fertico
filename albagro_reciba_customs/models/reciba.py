bf101c1b0ea02b32164ffcb087f59af501b1f4fc
CSE Sebastián Ayala Méndez <62626813+Sebastian-AM-Wobin@users.noreply.github.com>
2020-08-17 12:02:21 -0500

    Update reciba.py

--- albagro_reciba_customs/models/reciba.py
+++ albagro_reciba_customs/models/reciba.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
 
 class RecibaOrder(models.Model):
     _name = 'reciba.order'
@@ -26,5 +28,38 @@ class RecibaOrder(models.Model):
         for reciba in self:
             res.append((reciba.id,reciba.no_ticket))
         return res
+
+
+    @api.model
+    def display_debts_pop_up(self):
+        #Retrieve rows from "account.invoice" model where the present provider
+        #is revised to detect if has open invoices indicating that must pay its debts:         
+        sql_query = """SELECT company_id, SUM(residual_signed) 
+                         FROM account_invoice 
+                        WHERE (partner_id = %s)
+                          AND (state = 'open')
+                          AND (type = 'in_invoice' OR type = 'in_refund') 
+                        GROUP BY company_id;"""
+                        
+        self.env.cr.execute(sql_query, (self.customer_id.id,))
+        residual_companies = self.env.cr.fetchall()        
+
+        #Validate if query has results:
+        if residual_companies:           
+            #Construct the error message, beginning with client with open sales invoices:
+            debtor = self.env['res.partner'].search([('id', '=', self.partner_id.id)]).name
+            msg = _('The related contact on the purchase order %s has outstanding balances on sales: \n') % (debtor)
+                          
+            for val in residual_companies:
+                #Iterate companies with residual (outstanding balance) and concatenate
+                company_name = self.env['res.company'].search([('id', '=', val[0])]).name
+                msg += _('\n[+] %s balance by $%s') % (company_name, val[1])                    
+                #Sum up residuals in order to indicate how much is debted
+                summatory_residual += val[1] 
+                         
+            #Concatenate last part of error message:
+            msg += _('\n\nConsider making discounts for settlement.')
+        
+            raise UserError(msg)
 
 
