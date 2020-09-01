13819480ad420504acc9b5e2a4fffb2fc6b8940a
Noe Moreno <nmoreno@wobin.com.mx>
2020-09-01 12:47:04 -0500

    [ADD] logger comentado

--- albagro_reciba_customs/models/saldos_pendientes.py
+++ albagro_reciba_customs/models/saldos_pendientes.py
@@ -78,7 +78,7 @@ class PurchaseOrder(models.Model):
         self.ammount_pending_difference = self.ammount_sl_pending_inv - self.ammount_select_discounts
 
 
-    @api.onchange('pending_sales_invoices_ids')
+    '''@api.onchange('pending_sales_invoices_ids')
     def _onchange_pending_sales_invoices_ids(self):
         import logging; _logger = logging.getLogger(__name__)
           _logger.info('\n\n\n sí entra, mostrar contexto %s\n\n\n', self.env.context)
@@ -86,7 +86,7 @@ class PurchaseOrder(models.Model):
         for line in self.pending_sales_invoices_ids:
             if line.selected_sl_inv == True:        
                 dict_vals = {'assigned_pur_ord': self.id} 
-                line.write(dict_vals)
+                line.write(dict_vals)'''
 
 
     #/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
