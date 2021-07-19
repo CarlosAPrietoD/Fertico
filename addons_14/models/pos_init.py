41457d6b59f604ea14730657d9abf3322b77141c
Noe Moreno <nmoreno@wobin.com.mx>
2021-07-19 10:05:57 -0500

    [ADD] states

--- addons_14/models/pos_init.py
+++ addons_14/models/pos_init.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 class PosSession(models.Model):
     _inherit = 'pos.session'
 
-    pos_verify = fields.Boolean(string="Verificado")
+    pos_verify = fields.Selection([('verified','Verificado'),('unverified','Sin verificar')], string="Verificado", default='unverified')
 
     def action_pos_session_verified(self):        
-        self.pos_verify = True
+        self.pos_verify = 'verified'
